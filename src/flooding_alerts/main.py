ecab7a1cd438f5e01042d5020bf41eee24636459
barrygee <barry.mark.gee@icloud.com>
2024-05-17 11:07:58 +0100

    update

--- src/flooding_alerts/main.py
+++ src/flooding_alerts/main.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 from crew import CrewaiFloodingAlertsAndWarningsCrew
 
-
 def run():
     # Replace with your inputs, it will automatically interpolate any tasks and agents information
     inputs = {
         'topic': 'Flood alerts and warnings',
         'format': 'JSON',
-        'location': 'Trent',
+        'location': 'London',
     }
     result = CrewaiFloodingAlertsAndWarningsCrew().crew().kickoff(inputs=inputs)
     print(result)
